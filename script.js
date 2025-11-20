4479f9c0143900ac27a9083620a2914b6649d6b3
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-20 08:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 
+function validateName() {
+    const pattern = /^[A-Za-z]{3,}$/;
+    const input = document.getElementById("name");
+
+    if (!input.value) {
+        input.style.backgroundColor = "pink"; 
+    } else {
+        input.style.backgroundColor = pattern.test(input.value) ? "lightgreen" : "pink";
+    }
+}
+
+function validateEmail() {
+    const pattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    const input = document.getElementById("email");
+
+    if (!input.value) {
+        input.style.backgroundColor = "pink";
+    } else {
+        input.style.backgroundColor = pattern.test(input.value) ? "lightgreen" : "pink";
+    }
+}
+
+function validateMobile() {
+    const pattern = /^[0-9]{10}$/;
+    const input = document.getElementById("mobile");
+
+    if (!input.value) {
+        input.style.backgroundColor = "pink"; 
+    } else {
+        input.style.backgroundColor = pattern.test(input.value) ? "lightgreen" : "pink";
+    }
+}
